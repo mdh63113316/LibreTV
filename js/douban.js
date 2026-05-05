d26c6b8266f105af772f382ab5be5f8a7c35be63
mdh63113316 <1452359101@qq.com>
2026-05-05 15:35:26 +0800

    Change Douban API URLs to new endpoint
    Updated Douban API endpoints to use caiji.dbzy5.com instead of movie.douban.com.

--- js/douban.js
+++ js/douban.js
@@ -382,7 +382,8 @@ function setupDoubanRefreshBtn() {
 }
 
 function fetchDoubanTags() {
-    const movieTagsTarget = `https://movie.douban.com/j/search_tags?type=movie`
+    // const movieTagsTarget = `https://movie.douban.com/j/search_tags?type=movie`
+     const movieTagsTarget = `https://caiji.dbzy5.com/j/search_tags?type=movie`
     fetchDoubanData(movieTagsTarget)
         .then(data => {
             movieTags = data.tags;
@@ -393,7 +394,9 @@ function fetchDoubanTags() {
         .catch(error => {
             console.error("获取豆瓣热门电影标签失败：", error);
         });
-    const tvTagsTarget = `https://movie.douban.com/j/search_tags?type=tv`
+    // const tvTagsTarget = `https://movie.douban.com/j/search_tags?type=tv`
+    const tvTagsTarget = `https://caiji.dbzy5.com/j/search_tags?type=tv`
+    
     fetchDoubanData(tvTagsTarget)
        .then(data => {
             tvTags = data.tags;
@@ -423,7 +426,9 @@ function renderRecommend(tag, pageLimit, pageStart) {
     container.classList.add("relative");
     container.insertAdjacentHTML('beforeend', loadingOverlayHTML);
     
-    const target = `https://movie.douban.com/j/search_subjects?type=${doubanMovieTvCurrentSwitch}&tag=${tag}&sort=recommend&page_limit=${pageLimit}&page_start=${pageStart}`;
+    // const target = `https://movie.douban.com/j/search_subjects?type=${doubanMovieTvCurrentSwitch}&tag=${tag}&sort=recommend&page_limit=${pageLimit}&page_start=${pageStart}`;
+    const target = `https://caiji.dbzy5.com/j/search_subjects?type=${doubanMovieTvCurrentSwitch}&tag=${tag}&sort=recommend&page_limit=${pageLimit}&page_start=${pageStart}`;
+    
     
     // 使用通用请求函数
     fetchDoubanData(target)
@@ -451,7 +456,8 @@ async function fetchDoubanData(url) {
         signal: controller.signal,
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
-            'Referer': 'https://movie.douban.com/',
+            // 'Referer': 'https://movie.douban.com/',
+            'Referer': 'https://caiji.dbzy5.com/',
             'Accept': 'application/json, text/plain, */*',
         }
     };
